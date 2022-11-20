ad838fbfe07ce15900ac5a4d5d6cb774445aa25c
haribo14 <edcosy@gmail.com>
2022-11-20 21:23:00 +0900

    feature : 이미지 정보 반환 매핑

--- WYW-spring/src/main/java/com/project/WYW/controller/ProductController.java
+++ WYW-spring/src/main/java/com/project/WYW/controller/ProductController.java
@@ -1,7 +1,12 @@
 package com.project.WYW.controller;
 
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import com.project.WYW.model.AttachImageVO;
+import com.project.WYW.service.AttachService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
@@ -10,9 +15,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 
 @Controller
 public class ProductController {
+
+    @Autowired
+    private AttachService attachService;
+
+
 
     @GetMapping("/display")
     public ResponseEntity<byte[]> getImage(String fileName){
@@ -35,6 +46,19 @@ public class ProductController {
 
         return result;
 
+    }
+
+    /* 이미지 정보 반환 */
+    @GetMapping(value="/getAttachList", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<AttachImageVO>> getAttachList(int product_id){
+
+        return new ResponseEntity<List<AttachImageVO>>(attachService.getAttachList(product_id), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/productDetail")
+    public String productDtail(){
+        return "productDetail";
     }
 
 }
