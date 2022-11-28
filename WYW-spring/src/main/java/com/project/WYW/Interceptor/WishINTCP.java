a8f9303f320f5166cc31b78bd9175a2763bcd986
haribo14 <edcosy@gmail.com>
2022-11-28 23:02:16 +0900

    fix : 로그인이 필요한 서비스에 대한 인터셉터 적용

--- WYW-spring/src/main/java/com/project/WYW/Interceptor/WishINTCP.java
+++ WYW-spring/src/main/java/com/project/WYW/Interceptor/WishINTCP.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class CartINTCP implements HandlerInterceptor {
+public class WishINTCP implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
