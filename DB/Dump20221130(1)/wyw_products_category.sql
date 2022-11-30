8cb14bdda373975139abf6348f84d00334b05b15
osy8814 <111236111+osy8814@users.noreply.github.com>
2022-11-30 09:44:01 +0900

    chore : DB 외부키 이름 수정, dumps

---
+++ DB/Dump20221130(1)/wyw_products_category.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: localhost    Database: wyw
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `products_category`
+--
+
+DROP TABLE IF EXISTS `products_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products_category` (
+  `cate_name` varchar(20) NOT NULL,
+  `cate_code` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cate_code_ref` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`cate_code`),
+  KEY `products_category_ibfk_1` (`cate_code_ref`),
+  CONSTRAINT `products_category_ibfk_1` FOREIGN KEY (`cate_code_ref`) REFERENCES `products_category` (`cate_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products_category`
+--
+
+LOCK TABLES `products_category` WRITE;
+/*!40000 ALTER TABLE `products_category` DISABLE KEYS */;
+INSERT INTO `products_category` VALUES ('침실','100',NULL),('침실 책상','101','100'),('침실 의자','102','100'),('침실 선반','103','100'),('침실 침대','104','100'),('거실','200',NULL),('거실 책상','201','200'),('거실 의자','202','200'),('거실 선반','203','200'),('주방','300',NULL),('주방 책상','301','300'),('주방 의자','302','300'),('주방 선반','303','300');
+/*!40000 ALTER TABLE `products_category` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-30  9:42:39
