20973cd6fc22561db89536fd3c05df9875a30aeb
haribo14 <edcosy@gmail.com>
2022-12-02 03:34:58 +0900

    feat : 비동기화 방식으로 비밀번호 찾기 기능 구현
    이메일, 이름 입력 -> 메일로 인증번호 발송->인증번호 확인 후 입력 -> 일치하면 메일로 비밀번호 발송, 로그인창으로 이동

--- WYW-spring/src/main/java/com/project/WYW/controller/UsersController.java
+++ WYW-spring/src/main/java/com/project/WYW/controller/UsersController.java
@@ -2,16 +2,22 @@ package com.project.WYW.controller;
 
 
 import com.project.WYW.domain.UsersVo;
+import com.project.WYW.dto.FindPwdDto;
 import com.project.WYW.service.UsersSecvice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.Random;
 
 @Controller
 @RequestMapping("/users")
@@ -19,14 +25,19 @@ public class UsersController {
 
     @Autowired
     UsersSecvice usersSecvice;
+    @Autowired
+    JavaMailSenderImpl mailSender;
+
+    private String password;
+    private String email;
 
     @GetMapping("/login")
-    public String getLogin() throws Exception{
+    public String getLogin() throws Exception {
         return "login";
     }
 
-    @PostMapping ("/login.do")
-    public String postLogin(UsersVo vo, HttpServletRequest req, RedirectAttributes rttr) throws Exception{
+    @PostMapping("/login.do")
+    public String postLogin(UsersVo vo, HttpServletRequest req, RedirectAttributes rttr) throws Exception {
 
 
         UsersVo loginUser = null;
@@ -35,8 +46,8 @@ public class UsersController {
         HttpSession session = req.getSession();
         try {
             loginUser = usersSecvice.login(vo.getUserId());
-            if(loginUser.isActive()==false){
-                rttr.addFlashAttribute("msg","inactive");
+            if (loginUser.isActive() == false) {
+                rttr.addFlashAttribute("msg", "inactive");
                 return "redirect:/users/login.do";
             }
             loginUserPwd = loginUser.getPassword();
@@ -44,16 +55,16 @@ public class UsersController {
         } catch (Exception e) {
             e.printStackTrace();
             session.setAttribute("loggedInUser", null);
-            rttr.addFlashAttribute("inputId",vo.getUserId());
+            rttr.addFlashAttribute("inputId", vo.getUserId());
             rttr.addFlashAttribute("msg", "false");
             return "redirect:/users/login.do";
         }
 
-        if(loginUserPwd.equals(inputPass)){
+        if (loginUserPwd.equals(inputPass)) {
             session.setAttribute("loggedInUser", loginUser);
-        }else{
+        } else {
             session.setAttribute("loggedInUser", null);
-            rttr.addFlashAttribute("inputId",vo.getUserId());
+            rttr.addFlashAttribute("inputId", vo.getUserId());
             rttr.addFlashAttribute("msg", "false");
             return "redirect:/users/login.do";
         }
@@ -63,12 +74,12 @@ public class UsersController {
 
     @ResponseBody
     @PostMapping("/logout")
-    public void logout(HttpSession session) throws Exception{
+    public void logout(HttpSession session) throws Exception {
         session.invalidate();
     }
 
     @GetMapping("/signup")
-    public String getSignup() throws Exception{
+    public String getSignup() throws Exception {
         return "signUp/signUp";
     }
 
@@ -76,7 +87,7 @@ public class UsersController {
     public String postSignup(@Valid UsersVo vo, Model m) throws Exception {
         int isSuccessful = usersSecvice.singUp(vo);
 
-        if(0 < isSuccessful){
+        if (0 < isSuccessful) {
             m.addAttribute(vo);
             return "signUp/signUp_complete";
         }
@@ -85,7 +96,7 @@ public class UsersController {
     }
 
     @GetMapping("/mypage")
-    public String getMypage(){
+    public String getMypage() {
         return "myPage/mypage";
     }
 
@@ -98,12 +109,12 @@ public class UsersController {
     @PostMapping("/findid")
     public String findIdPost(UsersVo usersVo) {
         String userId = usersSecvice.findId(usersVo);
-        if(userId==null){
+        if (userId == null) {
             return "NOT_FOUND";
         }
         StringBuffer stringBuffer = new StringBuffer(userId);
-        for (int i = 2; i <userId.length()-2 ; i++) {
-            stringBuffer.replace(i,i+1,"*");
+        for (int i = 2; i < userId.length() - 2; i++) {
+            stringBuffer.replace(i, i + 1, "*");
         }
 
         return stringBuffer.toString();
@@ -115,13 +126,87 @@ public class UsersController {
     }
 
     @ResponseBody
+    @PostMapping("/sendMail")
+    public FindPwdDto sendMail(UsersVo usersVo) {
+
+        FindPwdDto findPwdDto = new FindPwdDto();
+
+        password = usersSecvice.findPwd(usersVo);
+        if (password == null) {
+            findPwdDto.setResultMsg("NOT_FOUND");
+            return findPwdDto;
+        }
+
+        Random random = new Random();
+
+        int certification = random.nextInt(8888) + 1111;
+        findPwdDto.setCertification(certification);
+        String subject = "WYW Mall : [비밀번호 찾기]인증번호";
+        String content =
+                "안녕하십니까? WYW Mall 입니다." +
+                "<br><br>" +
+                "고객님의 인증번호는<strong> " + certification + " </strong>입니다." +
+                "<br><br>" +
+                "정보찾기 창에 인증번호를 입력해 주십시오.";
+        String from = "wywsyhmall@gmail.com";
+        email = usersVo.getEmail();
+        String to = email;
+
+        try {
+            MimeMessage mail = mailSender.createMimeMessage();
+            MimeMessageHelper mailHelper = new MimeMessageHelper(mail, true, "UTF-8");
+
+            mailHelper.setFrom(from);
+            mailHelper.setTo(to);
+            mailHelper.setSubject(subject);
+            mailHelper.setText(content, true);
+
+            mailSender.send(mail);
+            return findPwdDto;
+        } catch (Exception e) {
+            e.printStackTrace();
+            findPwdDto.setResultMsg("SEND_FAIL");
+            return findPwdDto;
+        }
+    }
+    @ResponseBody
+    @PostMapping("/sendpass")
+    public int sendPass() {
+
+        String subject = "WYW Mall : [비밀번호 찾기]비밀번호";
+        String content =
+                "안녕하십니까? WYW Mall 입니다." +
+                "<br><br>" +
+                "고객님의 비밀번호는<strong> " + password + " </strong>입니다.";
+
+        String from = "wywsyhmall@gmail.com";
+        String to = email;
+
+        try {
+            MimeMessage mail = mailSender.createMimeMessage();
+            MimeMessageHelper mailHelper = new MimeMessageHelper(mail, true, "UTF-8");
+
+            mailHelper.setFrom(from);
+            mailHelper.setTo(to);
+            mailHelper.setSubject(subject);
+            mailHelper.setText(content, true);
+
+            mailSender.send(mail);
+            return 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    @ResponseBody
     @PostMapping("/idChk")
-    public boolean idChk(HttpServletRequest request) throws Exception{
+    public boolean idChk(HttpServletRequest request) throws Exception {
         String userId = request.getParameter("userId");
 
         UsersVo vo = usersSecvice.read(userId);
 
-        if(vo!=null){
+        if (vo != null) {
             return true;
         }
         return false;
@@ -129,29 +214,30 @@ public class UsersController {
 
     @ResponseBody
     @PostMapping("/emailChk")
-    public boolean emailChk(HttpServletRequest request) throws Exception{
+    public boolean emailChk(HttpServletRequest request) throws Exception {
         String email = request.getParameter("email");
         UsersVo vo = usersSecvice.emailChk(email);
 
-        if(vo!=null){
-            return true ;
+        if (vo != null) {
+            return true;
         }
         return false;
     }
 
     @ResponseBody
     @PostMapping("/mobileChk")
-    public boolean mobileChk(HttpServletRequest request) throws Exception{
+    public boolean mobileChk(HttpServletRequest request) throws Exception {
         String mobile1 = request.getParameter("mobile1");
         String mobile2 = request.getParameter("mobile2");
         String mobile3 = request.getParameter("mobile3");
-        String mobile = mobile1+"-"+mobile2+"-"+mobile3;
+        String mobile = mobile1 + "-" + mobile2 + "-" + mobile3;
 
         UsersVo vo = usersSecvice.mobileChk(mobile);
 
-        if(vo!=null){
+        if (vo != null) {
             return true;
         }
         return false;
     }
+
 }
--- WYW-spring/src/main/webapp/WEB-INF/views/find_pwd.jsp
+++ WYW-spring/src/main/webapp/WEB-INF/views/find_pwd.jsp
@@ -1,62 +1,129 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+         pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html lang="kr">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" href="${pageContext.request.contextPath}/img/WYWlogo.png" />
-    <link rel="stylesheet" href="${pageContext.request.contextPath}/css/style.css" />
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="icon" href="${pageContext.request.contextPath}/img/WYWlogo.png"/>
+    <link rel="stylesheet" href="${pageContext.request.contextPath}/css/style.css"/>
     <title>WYW</title>
     <style></style>
-  </head>
+</head>
 
-  <body>
-    <jsp:include page="index_top.jsp" flush="false"/>
-    <div class="find_pwd_main">
-      <div class="find_pwd_main-outter">
+<body>
+<jsp:include page="index_top.jsp" flush="false"/>
+<div class="find_pwd_main">
+    <div class="find_pwd_main-outter">
         <h1 class="find_pwd_title">정보찾기</h1>
         <form class="find_pwd_form">
-          <h1 class="find_pwd_form_title">
-            <i class="fas fa-fingerprint"></i>비밀번호 찾기
-          </h1>
+            <h1 class="find_pwd_form_title">
+                <i class="fas fa-fingerprint"></i>비밀번호 찾기
+            </h1>
 
-          <div class="find_pwd_form_inputAndBtn">
-            <div class="find_pwd_form_name_email">
-              <label for="find_pwd_form_email"
-                ><input
-                  type="email"
-                  id="find_pwd_form_email"
-                  name="email"
-                  placeholder="이메일"
-                  autofocus
-              /></label>
-              <label for="find_pwd_form_name"
-                ><input
-                  type="text"
-                  id="find_pwd_form_name"
-                  name="pwd"
-                  placeholder="이름"
-              /></label>
+            <div class="find_pwd_form_inputAndBtn">
+                <div class="find_pwd_form_name_email">
+                    <label for="find_pwd_form_email"
+                    ><input
+                            type="email"
+                            id="find_pwd_form_email"
+                            name="email"
+                            placeholder="이메일"
+                            autofocus
+                    /></label>
+                    <label for="find_pwd_form_name"
+                    ><input
+                            type="text"
+                            id="find_pwd_form_name"
+                            name="pwd"
+                            placeholder="이름"
+                    /></label>
+                </div>
+                <button type="button"  class="confirm" >비밀번호 찾기</button>
             </div>
-            <button id="confirm">비밀번호 찾기</button>
-          </div>
-          <ul class="find_pwd_findAndRegInfo">
-            <li><a href="/WYW/users/findid">아이디 찾기</a></li>
-            <li>|</li>
-            <li><a href="/WYW/users/findpwd">비밀번호 찾기</a></li>
-            <li>|</li>
-            <li><a href=/WYW/users/findpwd">회원가입</a></li>
-          </ul>
+            <div class="find_pwd_certification_number hidden_div">
+                <input id="certification_number" style="margin-right: 10px">
+                <button id="certification_btn" type="button">인증번호확인</button>
+            </div>
+            <ul class="find_pwd_findAndRegInfo">
+                <li><a href="/WYW/users/findid">아이디 찾기</a></li>
+                <li>|</li>
+                <li><a href="/WYW/users/signup">회원가입</a></li>
+            </ul>
         </form>
-      </div>
     </div>
-    <jsp:include page="index_bottom.jsp" flush="false"/>
+</div>
+<jsp:include page="index_bottom.jsp" flush="false"/>
 
-    <script
-      src="https://kit.fontawesome.com/6478f529f2.js"
-      crossorigin="anonymous"
-    ></script>
-  </body>
+<script
+        src="https://kit.fontawesome.com/6478f529f2.js"
+        crossorigin="anonymous"
+></script>
+<script>
+
+    let dto;
+
+    $(".confirm").on("click", function (){
+        const userEmail = $("#find_pwd_form_email").val();
+        const userName = $("#find_pwd_form_name").val();
+        let form = {
+            email: userEmail,
+            name: userName,
+        }
+
+        alert("메일 발송중입니다. 잠시만 기다려주세요.")
+        $(".confirm").attr("disabled",true);
+        $(".confirm").addClass("disabled_btn");
+        $(".find_pwd_certification_number").removeClass("hidden_div");
+        $("#certification_btn").attr("disabled",true);
+        $.ajax({
+            url: '/WYW/users/sendMail',
+            type: 'POST',
+            data: form,
+            dataType: "json",
+            success: function (result) {
+                dto = result;
+                $(".confirm").attr("disabled",false);
+                $(".confirm").removeClass("disabled_btn");
+                $("#certification_number").attr("disabled",true);
+                if (dto.resultMsg == "NOT_FOUND") {
+                    alert("일치하는 정보가 없습니다.")
+                }else if(dto.resultMsg == "SEND_FAIL"){
+                    alert("메일 발송에 실패 하였습니다. 다시 시도해주세요")
+                }else {
+                    alert("인증번호가 발송되었습니다.");
+                    $("#certification_number").attr("disabled",false);
+                    $("#certification_btn").attr("disabled",false);
+                }
+            }
+        });
+    });
+
+    $("#certification_btn").on("click", function (){
+        let inputNumber = $("#certification_number").val();
+        console.log(inputNumber);
+        console.log(dto.certification);
+        if(inputNumber==dto.certification){
+            alert("인증번호가 일치합니다. 메일이 발송됩니다.")
+            $.ajax({
+                url: '/WYW/users/sendpass',
+                type: 'POST',
+                dataType: "json",
+                success: function (result) {
+                    if(result==1){
+                        alert("비밀번호가 메일로 발송되었습니다. 로그인 페이지로 이동합니다.");
+                        window.location.replace("/WYW/users/login.do");
+                    }else {
+                        alert("메일 발송에 실패하였습니다. 다시 시도해주세요.")
+                    }
+
+                }
+            });
+        }else {
+            alert("인증번호가 일치하지 않습니다. 다시 확인해주세요.")
+        }
+    });
+</script>
+</body>
 </html>
