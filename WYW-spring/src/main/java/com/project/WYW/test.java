37b9ca55926084ea4edfdc57cea86be1ae3ec5b6
haribo14 <edcosy@gmail.com>
2022-12-01 14:25:08 +0900

    test : 아이디 가림처리 테스트

--- WYW-spring/src/main/java/com/project/WYW/test.java
+++ WYW-spring/src/main/java/com/project/WYW/test.java
@@ -5,11 +5,12 @@ import java.util.regex.Pattern;
 public class test {
 
     public static void main(String[] args) {
-
-//        String Regex = "[A-Za-z0-9]{4,16}";
-        String Regex = "^((?=.*\\d)(?=.*[a-zA-Z])(?=.*[\\W]).{8,})$";
-        boolean result = Pattern.matches(Regex, "dDdd1234@");
-
-        System.out.println(result);
+        String userId ="adsflkajlkfdmin";
+        StringBuffer stringBuffer = new StringBuffer(userId);
+        for (int i = 2; i <userId.length()-2 ; i++) {
+            stringBuffer.replace(i,i+1,"*");
+        }
+        System.out.println("stringBuffer = " + stringBuffer);
+        
     }
 }
